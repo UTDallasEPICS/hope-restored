6766499c7471eb2d13803247d0ef58bc4f3e29c7
Jayden-Boomer <89496979+Jayden-Boomer@users.noreply.github.com>
2025-02-27 22:00:17 -0600

    updated grid for auth page

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -3,7 +3,7 @@
   <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
     
     <h1 class="text-3xl font-bold mb-8">Admin Homepage</h1>
-    <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
+    <div class="grid grid-cols-1 gap-6 md:grid-cols-1">
       <SignUpLoginTab />
       <SettingsTab />
     </div>
