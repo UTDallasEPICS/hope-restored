0bc217daeab75b19e23b96d2092af9e8adbfb65c
Jayden-Boomer <89496979+Jayden-Boomer@users.noreply.github.com>
2025-03-13 21:35:27 -0500

    added app.css

--- webcomponent/wordpress.ts
+++ webcomponent/wordpress.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import Wordpress from "./src/views/Resource.vue";
-import "./index.css";
+import "./src/App.css";
 
 const app = createApp(Wordpress);
 app.mount("#app");
