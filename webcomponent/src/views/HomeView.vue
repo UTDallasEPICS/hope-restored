26776c86e26d550159dd69a18dc595a3ad70d78f
Jayden-Boomer <89496979+Jayden-Boomer@users.noreply.github.com>
2025-04-17 16:15:51 -0500

    filter deck sidebar doesn't scroll with the resources anymore

--- webcomponent/src/views/HomeView.vue
+++ webcomponent/src/views/HomeView.vue
@@ -5,11 +5,21 @@ import PublicLandingModal from "../components/PublicLandingModal.vue";
 </script>
 
 <template>
-  <div id="scroll-to-top-anchor">
-    <PublicLandingModal />
-    <div class="flex flex-row flex-auto">
-      <div class="flex flex-none w-64"><FilterDeck /></div>
-      <div class="flex grow-[1]"><ResourceDeck /></div>
+  <div class="flex h-screen overflow-hidden fixed">
+    <!-- Sidebar -->
+    <div class="w-64 flex-shrink-0 border-r border-gray-200 bg-white">
+      <FilterDeck />
+    </div>
+
+    <!-- Main Content -->
+    <div class="flex-1 overflow-y-auto">
+      <div id="scroll-to-top-anchor">
+        <PublicLandingModal />
+        <ResourceDeck />
+      </div>
     </div>
   </div>
 </template>
+
+
+
