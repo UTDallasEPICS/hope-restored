7c3a2a8524db8591a09d9796bc3885d61879828d
ablue190 <emailabhibulusu@gmail.com>
2025-10-02 16:47:34 -0500

    Adding landing page for Reports section

--- admin/pages/reports.vue
+++ admin/pages/reports.vue
@@ -4,3 +4,19 @@
         <h1>  </h1>
     </div>
 </template>
+<script>
+export default {
+    name: 'ReportsLandingPage'
+}
+</script>
+
+<style scoped>
+.reports-landing {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #f5f6fa;
+}
+
+</style>
