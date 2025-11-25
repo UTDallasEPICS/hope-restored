53a4129898e6e47131321de3523a855d47c9da78
fegelsamuel <fegelsamuel@gmail.com>
2025-11-24 21:30:28 -0600

    [nuxi 9:30:19 PM]  ERROR  Nuxt Build Error: Could not resolve "../components/Inventory/ItemsPerCategoryChart.vue" from "pages/inventoryBackup.vue?vue&type=script&setup=true&lang.ts" file: /Users/vergil/Documents/Repos/hope-restored/admin/pages/inventoryBackup.vue?vue&type=script&setup=true&lang.ts
        at getRollupError (node_modules/rollup/dist/es/shared/parseAst.js:401:41)
        at error (node_modules/rollup/dist/es/shared/parseAst.js:397:42)
        at ModuleLoader.handleInvalidResolvedId (node_modules/rollup/dist/es/shared/node-entry.js:21590:24)
        at node_modules/rollup/dist/es/shared/node-entry.js:21550:26
    
     ELIFECYCLE  Command failed with exit code 1.

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -1,34 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config'
-import tailwindcss from "@tailwindcss/vite";
 
 
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
-  vite: {
-  plugins: [
-    '~/qr-code-stuff/vue3-qr-reader.client.ts',
-    tailwindcss({
-      src: [
-        './app.vue',
-        './components/**/*.{vue,js,ts}',
-        './layouts/**/*.{vue,js,ts}',
-        './pages/**/*.{vue,js,ts}',
-        './plugins/**/*.{js,ts}',
-        ]
-      }),
-    ],
-  },
+  css: ['~/assets/css/main.css'],
+  plugins: [ '~/qr-code-stuff/vue3-qr-reader.client.ts' ],
+
   components:{
     dirs: [
       "@/components/"
     ]
   },
+
   // Redirect root path to Home to effectively disable pages/index.vue
   routeRules: {
     '/': { redirect: '/Home' }
   },
-  compatibilityDate: "2025-02-27"
+
+  compatibilityDate: "2025-02-27",
+  modules: ['@nuxtjs/tailwindcss']
 })
