836725e62f48ab9b265549e54893ba805b9a4012
Vikas Thoutam <vikas.thoutam@gmail.com>
2026-08-23 17:56:21 -0400

    chore: update prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,7 @@
-import "dotenv/config";
+import dotenv from 'dotenv'
 import { defineConfig, env } from "prisma/config";
+
+dotenv.config()
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -8,6 +10,6 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL || 'file:./prisma/dev.db',
   },
 });
